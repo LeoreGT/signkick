ff9b178e49ee5009dca5364841c2a058037ae7cc
claudia mancuso <claudiar.mancuso@gmail.com>
2016-11-23 17:22:08 +0100

    updated component changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :bookings
   end
   get 'search', to: "interpreters#search"
+  get 'design', to: "pages#design"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
