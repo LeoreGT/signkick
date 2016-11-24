10db94fd6e1266ff4dd89928760292e106ed3e0c
Nicolas Feer <atmosfeer@gmail.com>
2016-11-24 12:14:06 +0100

    Sign up skeleton in place

--- app/controllers/interpreters_controller.rb
+++ app/controllers/interpreters_controller.rb
@@ -8,4 +8,21 @@ class InterpretersController < ApplicationController
     @interpreter = Interpreter.find(params[:id])
   end
 
+  def new
+    @interpreter = Interpreter.new
+  end
+
+  def create
+    interpreter = Interpreter.new(interpreter_params)
+    interpreter.user = current_user
+    interpreter.save
+    redirect_to interpreter_path(interpreter)
+  end
+
+  private
+
+  def interpreter_params
+    params.require(:interpreter).permit(:name, :bio, :location, :years_of_experience, :price)
+  end
+
 end
