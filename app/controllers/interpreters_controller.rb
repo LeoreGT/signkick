b22e9425410a32b493cde52c447c525ab7c13681
Leore Tobias <leoretobias@hotmail.com>
2016-11-24 10:59:39 +0100

    Booking Partial on Interpreter Showpage

--- app/controllers/interpreters_controller.rb
+++ app/controllers/interpreters_controller.rb
@@ -8,4 +8,38 @@ class InterpretersController < ApplicationController
     @interpreter = Interpreter.find(params[:id])
   end
 
+  def new
+    @interpreter = Interpreter.new()
+  end
+
+  def create
+    @interpreter = Interpreter.new(interpreter_params)
+    if @interpreter.save
+      redirect_to dashboard_interpreter_path(@interpreter)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @interpreter.update(interpreter_params)
+      redirect_to dashboard_interpreter_path(@interpreter)
+    else
+      render :new
+    end
+  end
+
+  def dashboard
+    @interpreter = Interpreter.find(params[:id])
+  end
+
+
+private
+
+  def interpreter_params
+    params.require(:interpreter).permit(:name, :bio, :price, :years_of_experience, :tag_id, :language_id, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'interpreters#search'
   devise_for :users
-  resources :interpreters, only: [:show, :index] do
+  resources :interpreters, only: [:show, :index, :new, :edit] do
     resources :bookings
   end
   get 'search', to: "interpreters#search"
   get 'design', to: "pages#design"
+  get 'dashboard', to: "interpreters#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
